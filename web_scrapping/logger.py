efcf692aa165f39e8227a05e92da09f9023c8720
Hang Tan Tai <htt31124@gmail.com>
2025-03-15 14:56:13 +0700

    feat: log and singleton pattern

--- web_scrapping/logger.py
+++ web_scrapping/logger.py
@@ -1,46 +1,48 @@
-import logging 
 import os
+import logging 
 from datetime import datetime
 from typing import Optional
 
 class Logger:
-    # what is called?
+    # instance storage: private class variable stores the single instance
     _instance = None
 
-    # what is that? cls?
-    def __new__(cls):
+    # overiding method
+    def __new__(cls, *args, **kwargs): # cls: class itself (Logger)
         if cls._instance is None:
             cls._instance = super().__new__(cls)
         return cls._instance
     
+    # lazy initialization
     def __init__(self):
         if not hasattr(self, "logger"):
-            # why need to do it?
+            # Create a logger instance 
             self.logger = logging.getLogger("linkedin_scapper")
             self._setup_logger()
     
     def _setup_logger(self):
         """Set up logging configuration"""
-        # what is that?
+        # level in logger: DEBUG < INFO < WARNING < ERROR < CRITICAL
+        # set minimum severity level for this logger to process message -> DEBUG will be ignored
         self.logger.setLevel(logging.INFO)
         
         # Creates logs directory if it doesn't exist
         log_dir = os.path.join(os.path.dirname(__file__), "logs")
-        os.makedirs(log_dir, exist_ok=True) # what is attribute exist_ok ?
+        os.makedirs(log_dir, exist_ok=True) # prevents errors if the directory already exists
     
         # File handler with current date
         log_file = os.path.join(log_dir, f"scrapper_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log")
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setLevel(logging.INFO) # why need to set level?
+        file_handler = logging.FileHandler(log_file) # write a log message to this file
+        file_handler.setLevel(logging.INFO) 
         
-        # Console handler, why need console?
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO) # why need to set level?
+        # Console handler
+        console_handler = logging.StreamHandler() # send log messgae to the console (terminal)
+        console_handler.setLevel(logging.INFO)
         
-        # Set formatter
+        # Set formatter, format for file log
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        file_handler.setFormatter(formatter) # why need to set format
-        console_handler.setFormatter(formatter) # why need to set format
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
         
         # Add handlers to logger
         self.logger.addHandler(file_handler)
@@ -48,7 +50,7 @@ class Logger:
     
     def info(self, message: str, extra: Optional[dict] = None):
         """Log error level message"""
-        self.logger.info(message, extra = extra) # what is extra?
+        self.logger.info(message, extra = extra)
     
     def error(self, message:str, extra: Optional[dict] = None):
         """Log error level message"""
@@ -63,5 +65,6 @@ class Logger:
         self.logger.debug(message, extra = extra)
 
 # Create a singleton instance
+# create a variable logger that holds the singleton instance
+# just declare variable for another file rather than the Logger class directly
 logger = Logger()
-# what is singleton instancce? why need to use it?
